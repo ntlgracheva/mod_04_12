bfadc5c0b75e5325aa686af2624e474596c3881e
Natalia Gracheva <ntlgracheva@gmail.com>
2023-05-31 23:27:21 +0200

    errors

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 const figures = [["камень", "ножницы", "бумага"], ["rock", "scissors", "paper"]];
-const gameText = [["Введите один из вариантов: камень, ножницы, бумага", "Please, enter your choice: rock, scissors, paper"]]
+const gameText = [["Введите один из вариантов: камень, ножницы, бумага", "Please, enter your choice: rock, scissors, paper"],
+["Вы хотите завершить игру?", "Do you want to leave the game?"]]
 const gameRules = [
     {
         "камень": {
@@ -48,11 +49,16 @@ const lang = langChoice.toUpperCase() === "EN" || langChoice.toUpperCase() === "
 console.log(`langChoice = ${langChoice}`);
 console.log(`lang = ${lang}`);
 
-let user = "123";          //вариант пользователя
-let computer = "";      //вариант компьютера   
+let user = null;                     //вариант пользователя
+let computer = null;                 //вариант пользователя
 console.log(`user = ${user}`);
+console.log(`computer = ${computer}`);
 
-//запрос варианта у пользователя
+let compScore = 0;                          // счетчик баллов компьютера
+let userScore = 0                           // счетчик баллов пользователя
+
+
+//функция запроса варианта у пользователя
 const userInput = () => {
     const userInput = prompt(gameText[0][lang]);
 
@@ -60,18 +66,51 @@ const userInput = () => {
         let input = userInput.trim().toLowerCase(); //удаляем пробелы, приводим в нижний регистр
         let inputLength = input.length;             //вычисляем длину строки, введенной пользователем
 
-        //если длина больше 0 (то есть пользователь ввел какие-то данные() то проверяем их, в ином случае, повторяем запрос
+        //если длина больше 0 (то есть пользователь ввел какие-то данные() то проверяем их
         if (inputLength > 0) {
+            // *** КАК ИЗБЕЖАТЬ ЦИКЛА????
             for (let i = 0; i < 3; i++) {
-                if (figures[lang][i].indexOf(input) === 0) { return user = figures[lang][i] }
+                console.log(`figures[lang][i].startsWith(input)  - ${figures[lang][i].startsWith(input)}`);
+                if (figures[lang][i].startsWith(input)) {
+                    console.log(`!!!${figures[lang][i]}`);
+                    return user = figures[lang][i]
+                }
+                // *** если введено неверное слово, то нужно возобновить цикл, а у меня  НЕ вызывается функция - userInput is not a function????
                 else { userInput() }
             }
         }
-        else { userInput() }
+        //если длина ввода пользователя 0, то выводим запрашиваем о завершении игры
+        let confirm = prompt(gameText[1][lang]);
+
+        //*** Как правильно обработать завершение игры? если пользователь ничего не введет (null) - выводим результат, а как обработать нажатие "отмены"?
+
     }
 
     return user = inputCheck(userInput);
 }
 
+//функция запроса варианта у компьютера
+const computerInput = () => {
+    const random = Math.floor(Math.random() * 9 + 1);
+    if (random <= 3) {
+        computer = figures[lang][0];
+    } else if (random <= 6) {
+        computer = figures[lang][1];
+    } else {
+        computer = figures[lang][2];
+    }
+    return computer;
+}
+
+
 user = userInput();
-console.log(`user = ${user}`);
+console.log(`input user = ${user}`);
+
+computer = computerInput();
+console.log(`input computer = ${computer}`);
+
+userScore += gameRules[lang].user.computer // считаем баллы пользователя
+compScore += gameRules[lang].computer.user // считаем баллы компьютера
+
+console.log(`userScore = ${userScore}`);
+console.log(`compScore = ${compScore}`);
